802672f4c563e9ee47bf230033d8337525a3493d
Yash Tyagi <y.tyagi@tcu.edu>
2022-08-24 10:09:03 -0500

    Java Code
    Runtime: 2ms
    Memory Usage: 42.3 MB

---
+++ 20. Valid Parenthesis/isValid.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> S = new Stack<>();
+
+        for (char c : s.toCharArray()){
+            if(s.length()%2 != 0) return false;
+            if(c == '(' || c == '{' || c == '[') S.push(c);
+            else {
+                if(S.isEmpty()) return false;
+                else{
+                    if(c == ')' && S.peek() == '(') S.pop();
+                    else if(c == '}' && S.peek() == '{') S.pop();
+                    else if(c == ']' && S.peek() == '[') S.pop();
+                    else return false;
+                }
+            }
+        }
+
+        if (S.isEmpty()) return true;
+        return false;
+    }
+}
